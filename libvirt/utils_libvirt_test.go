3d6724532d8ce0ce45d483d0014e7a4ee543bb9f
Dean Smith <dean@zelotus.com>
2017-09-08 12:49:37 +0100

    Full support for arch,machine & emulator settings
    Add emulator setting support
    
    Completes support from arch and machine when reading current status
    
    Adds a new function newDomainDefForConnection that takes
    a domainDef and populates with defaults from the current host
    which is more sensible than guessing across distributions
    
    Maps canonical machine types <> actual to avoid changes
    
    Due to the way libvirt works if you specific machine types like pc,isapc
    or q35 these get translated into 'canonical' forms, ie. the latest
    version
    
    When quering terraform will notice a change, as it would still be
    looking for pc and will get something like pc-i440fx-2.9.
    Code has been added to assume that if we see 'pc-i440fx-2.9' and it is
    canonical for pc then we translate back to pc for terraform.
    This could cause issues if you are specifically using pc-i440fx-2.9 as
    the machine type in your terraform file.

--- libvirt/utils_libvirt_test.go
+++ libvirt/utils_libvirt_test.go
@@ -4,7 +4,9 @@ import (
 	"encoding/xml"
 	"testing"
 
+	libvirt "github.com/libvirt/libvirt-go"
 	"github.com/libvirt/libvirt-go-xml"
+	"os"
 )
 
 func TestGetHostXMLDesc(t *testing.T) {
@@ -31,4 +33,82 @@ func TestGetHostXMLDesc(t *testing.T) {
 	if dd.Name != name {
 		t.Errorf("expected name %s, got %s", name, dd.Name)
 	}
+}
+
+func connect(t *testing.T) *libvirt.Connect {
+	conn, err := libvirt.NewConnect(os.Getenv("LIBVIRT_DEFAULT_URI"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return conn
+}
+
+func TestGetHostArchitecture(t *testing.T) {
+
+	conn := connect(t)
+	defer conn.Close()
+
+	arch, err := getHostArchitecture(conn)
+
+	if err != nil {
+		t.Errorf("error %v", err)
+	}
+
+	t.Logf("[DEBUG] arch - %s", arch)
+
+	if arch == "" {
+		t.Errorf("arch is blank.")
+	}
+}
+
+func TestGetCanonicalMachineName(t *testing.T) {
+
+	conn := connect(t)
+	defer conn.Close()
+	arch := "x86_64"
+	virttype := "hvm"
+	machine := "pc"
+
+	caps,err := getHostCapabilities(conn)
+	if err != nil {
+		t.Error(err)
+	}
+	
+	name, err := getCanonicalMachineName(caps, arch, virttype, machine)
+
+	if err != nil {
+		t.Errorf("Could not get canonical name for %s/%s", arch, machine)
+		return
+	}
+
+	t.Logf("Canonical name for %s/%s = %s", arch, machine, name)
+
+}
+
+func TestGetOriginalMachineName(t *testing.T) {
+	conn := connect(t)
+	defer conn.Close()
+	arch := "x86_64"
+	virttype := "hvm"
+	machine := "pc"
+
+	caps,err := getHostCapabilities(conn)
+	if err != nil {
+		t.Error(err)
+	}
+
+	canonname, err := getCanonicalMachineName(caps, arch, virttype, machine)
+	if err != nil {
+		t.Error(err)
+	}
+	reversename, err := getOriginalMachineName(caps, arch, virttype, canonname)
+	if err != nil {
+		t.Error(err)
+	}
+	if reversename != machine {
+		t.Errorf("Cannot reverse canonical machine lookup")
+	}
+
+	t.Logf("Reverse canonical lookup for %s is %s which matches %s", canonname, reversename, machine)
 }
